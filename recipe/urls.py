ffe348194f859c296d69d413504c074e2d1bbff4
Qu33nB <bdcotton52@gmail.com>
2020-05-05 14:13:35 -0500

    Updated routing functionality in views.py and urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from recipe import views
 
 urlpatterns = [
-    path('', views.index)
-    # path('admin/', admin.site.urls),
+    path('', views.index),
+    path('recipe/', views.recipe),
+    path('author/<int:id>', views.author)
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-from recipe.models import RecipeItem
+from recipe.models import RecipeItem, Author
 # Create your views here.
 
 
 def index(request):
     data = RecipeItem.objects.all()
     return render(request, 'index.html', {'data': data})
+
+
+def recipe(request):
+    data = RecipeItem.objects.all()
+    return render(request, 'recipe.html', {'data': data})
+
+
+def author(request):
+    data = Author.objects.all()
+    return render(request, 'author.html', {'data': data})
